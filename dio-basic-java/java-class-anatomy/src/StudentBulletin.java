ca8bf96bb78cbfc0b725f6290ee5de42297bee56
Jefferson Santana <jeffersonsantana1914@outlook.com>
2024-05-27 14:30:57 -0300

    fix: removed a Submodule folder

---
+++ dio-basic-java/java-class-anatomy/src/StudentBulletin.java
@@ -0,0 +1,10 @@
+public class StudentBulletin {
+  public static void main(String[] args) {
+    int finalMedia = 6;
+    if (finalMedia < 6)
+      System.out.println("Disapproved");
+    else if (finalMedia == 6)
+      System.out.println("Approved");  
+  }
+  
+}
