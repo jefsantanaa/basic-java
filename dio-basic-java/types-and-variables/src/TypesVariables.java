3cf75e5d65a33f966930fbcc3e2daa1e8aef89c5
Jefferson Santana <jeffersonsantana1914@outlook.com>
2024-05-30 03:48:11 -0300

    First class on variables

--- dio-basic-java/types-and-variables/src/TypesVariables.java
+++ dio-basic-java/types-and-variables/src/TypesVariables.java
@@ -1,5 +1,11 @@
 public class TypesVariables {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double minimumSalary = 2500;
+
+        short smallNumber = 1;
+        int normalNumber = smallNumber;
+        short smallNumber2 = (short) normalNumber;
+
+        final double PI_VALUE = 3.14; // We need to use the final word at the beginning of the declaration of a constant. 
     }
 }
